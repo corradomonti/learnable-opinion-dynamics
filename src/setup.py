d0e08b34d7680afdb0063ef216abe0c0f4eeabb5
Corrado <corrado.mont@gmail.com>
2020-05-19 00:56:54 +0200

    Installation script

---
+++ src/setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+setuptools.setup(
+    name="learnable-opinion-dynamics-model",
+    version="0.1",
+    author="Corrado Monti",
+    author_email="me@corradomonti.com",
+    description="Learnable opinion dynamics model",
+    long_description="Model presented in 'Learning Opinion Dynamics From Social Traces', KDD2020.",
+    url="https://github.com/corradomonti/learnable-opinion-dynamics",
+    packages=setuptools.find_packages(),
+    python_requires='>=3.5',
+)
